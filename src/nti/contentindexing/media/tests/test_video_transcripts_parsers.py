bc2748f24cb84721a816061388ab5675459a47ef
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-10-12 04:33:35 +0000

    coverage tests
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentindexing/trunk@123052 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentindexing/media/tests/test_video_transcripts_parsers.py
+++ src/nti/contentindexing/media/tests/test_video_transcripts_parsers.py
@@ -13,6 +13,8 @@ from hamcrest import none
 from hamcrest import is_not
 from hamcrest import has_length
 from hamcrest import assert_that
+from hamcrest import greater_than
+from hamcrest import has_property
 
 import os
 import codecs
@@ -32,13 +34,24 @@ class TestVideoTranscriptParser(ContentIndexingLayerTest):
         parser = component.getUtility(IVideoTranscriptParser, name="srt")
         with open(path, "r") as source:
             transcript = parser.parse(source)
+
         assert_that(transcript, is_not(none()))
         assert_that(str(transcript), is_not(none()))
         assert_that(repr(transcript), is_not(none()))
         assert_that(transcript, has_length(167))
+        assert_that(transcript,
+                    has_property('text', has_length(greater_than(6700))))
+        
         for e in transcript:
             assert_that(e, is_not(none()))
             assert_that(e.transcript, is_not(none()))
+            assert_that(str(e), is_not(none()))
+            assert_that(repr(e), is_not(none()))
+        
+        # add new entry
+        transcript.append(e)
+        assert_that(transcript[len(transcript) -1],
+                    is_(e))
 
     def test_sbv_parser(self):
         path = os.path.join(os.path.dirname(__file__),
