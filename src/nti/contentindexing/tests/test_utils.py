c09e3054a5c4dbc3392e0c3bfc3e136646f307af
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-10-12 03:13:27 +0000

    use starts_with
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentindexing/trunk@123043 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentindexing/tests/test_utils.py
+++ src/nti/contentindexing/tests/test_utils.py
@@ -10,6 +10,7 @@ from __future__ import absolute_import
 
 from hamcrest import is_
 from hamcrest import assert_that
+from hamcrest import starts_with
 from hamcrest import less_than_or_equal_to
 
 import unittest
@@ -27,8 +28,8 @@ class TestUtils(unittest.TestCase):
 
     def test_videotimestamp_to_text(self):
         f = 1321391468.413528
-        assert_that(date_to_videotimestamp(f), is_('15:11:08.413'))
-        assert_that(date_to_videotimestamp(str(f)), is_('15:11:08.410'))
+        assert_that(date_to_videotimestamp(f), starts_with('15:11:08.4'))
+        assert_that(date_to_videotimestamp(str(f)), starts_with('15:11:08.4'))
         assert_that(date_to_videotimestamp(None), is_(u''))
 
     def test_get_datetime(self):
